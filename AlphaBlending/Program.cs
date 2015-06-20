308babffd36dffb3df19dbb1bea9f2564e010a13
AlphaDelta <admin@skid.pro>
2015-06-20 14:27:13 +1200

    Added project

---
+++ AlphaBlending/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+class AlphaBlending
+{
+    [STAThread]
+    public static void Main()
+    {
+        Application.Run(new MyForm());
+    }
+}
